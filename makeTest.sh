b2ff7544de0f06a115141aa041c1915745b583fd
lulucasono <lulucasono@gmail.com>
2018-11-13 14:15:28 +0100

    fix command order

--- makeTest.sh
+++ makeTest.sh
@@ -2,6 +2,6 @@
 # create test set
 ls Data/Covtype > testSet.txt
 echo launch tests
-./test.sh < testSet.txt | tee output.txt | grep -E "^Temps|^Test|^testing" > result.txt |  awk '/^testing file/{line=$3","}/^Temps moyen apprentissage/{line=line""$5}/^Temps moyen analyse/{line=line","$5;}/^Test/{print line","$4;}'
+./test.sh < testSet.txt | tee output.txt | grep -E "^Temps|^Test|^testing" |  awk '/^testing file/{line=$3","}/^Temps moyen apprentissage/{line=line""$5}/^Temps moyen analyse/{line=line","$5;}/^Test/{print line","$4;}' > result.txt 
 
 #spark-shell -i <(echo val fileName = \"Data/Covtype/covtype32\" ; cat test.scala)
