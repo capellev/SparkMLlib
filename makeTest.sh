8daa36e357dbd3bc173a04ba571accc9f8f95059
lulucasono <lulucasono@gmail.com>
2018-11-13 14:14:21 +0100

    format to one line

--- makeTest.sh
+++ makeTest.sh
@@ -2,6 +2,6 @@
 # create test set
 ls Data/Covtype > testSet.txt
 echo launch tests
-./test.sh < testSet.txt | tee output.txt | grep -E "^Temps|^Test|^testing" > result.txt
+./test.sh < testSet.txt | tee output.txt | grep -E "^Temps|^Test|^testing" > result.txt |  awk '/^testing file/{line=$3","}/^Temps moyen apprentissage/{line=line""$5}/^Temps moyen analyse/{line=line","$5;}/^Test/{print line","$4;}'
 
 #spark-shell -i <(echo val fileName = \"Data/Covtype/covtype32\" ; cat test.scala)
