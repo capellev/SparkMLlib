82cd9f6826ae010d47444b15e77bed401b496124
lulucasono <lulucasono@gmail.com>
2018-11-13 11:28:14 +0100

    get only important lines

--- makeTest.sh
+++ makeTest.sh
@@ -2,5 +2,6 @@
 # create test set
 ls Data/Covtype > testSet.txt
 echo launch tests
-./test.sh < testSet.txt | tee output.txt
+./test.sh < testSet.txt | tee output.txt | grep -E "^Temps|^Test|^testing" > result.txt
+
 #spark-shell -i <(echo val fileName = \"Data/Covtype/covtype32\" ; cat test.scala)
